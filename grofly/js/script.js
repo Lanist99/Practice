d6e056d4ee592a6aaa99de72056433bca648d6f7
Lanist99 <danilevic99@gmail.com>
2022-12-20 22:42:51 +0100

    navigation

---
+++ grofly/js/script.js
@@ -0,0 +1,11 @@
+"use strict"
+
+document.addEventListener('click', documentClick);
+
+function documentClick(e) {
+    const targetItem = e.target;
+
+    if (targetItem.closest('.icon-menu')) {
+        document.documentElement.classList.toggle('menu-open');
+    }
+}
