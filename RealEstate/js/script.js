72dc89cda8aa43ccd4313f0b11570ecc8c6db671
Lanist99 <danilevic99@gmail.com>
2023-01-03 14:23:47 +0100

    added page

---
+++ RealEstate/js/script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.slider-main-block', {
+    // Optional parameters
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: 'body-main-block__arrow.swiper-button-next',
+      prevEl: 'body-main-block__arrow.swiper-button-prev',
+    },
+  
+  });
