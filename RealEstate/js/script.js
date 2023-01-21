ba68b73b8d289ea4bd12c4ba251966d04c4a8006
Lanist99 <danilevic99@gmail.com>
2023-01-21 22:52:53 +0100

    added tabs

--- RealEstate/js/script.js
+++ RealEstate/js/script.js
@@ -8,3 +8,26 @@ const swiper = new Swiper('.slider-main-block', {
       prevEl: '.swiper-button-prev',
     },
   });
+  // tabs
+  const tabNavItems = document.querySelectorAll(".tabs-deals__button");
+  const tabItems = document.querySelectorAll(".item-tabs");
+  document.addEventListener("click", function (e) {
+    const targetElement = e.target;
+    let currentActiveIndex = null;
+    let newActiveIndex = null;
+    if (targetElement.closest(".tabs-deals__button")) {
+      tabNavItems.forEach((tabNavItem, index) => {
+        if (tabNavItem.classList.contains("active")) {
+          currentActiveIndex = index;
+          tabNavItem.classList.remove("active");
+        }
+        if (tabNavItem === targetElement) {
+          newActiveIndex = index;
+        }
+        
+      });
+      targetElement.classList.add("active");
+      tabItems[currentActiveIndex].classList.remove("active");
+      tabItems[newActiveIndex].classList.add("active");
+    }  
+  });
